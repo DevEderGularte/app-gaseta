d05a34e69addb62aa17ff1544802b7dc1fe54b29
gularte <edergularte@gmail.com>
2024-12-12 14:12:12 -0300

    Projeto GasEta antes da limpeza

---
+++ app/src/main/java/devandroid/edergularte/appgaseta/view/MainActivity.java
@@ -0,0 +1,108 @@
+package devandroid.edergularte.appgaseta.view;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import devandroid.edergularte.appgaseta.R;
+import devandroid.edergularte.appgaseta.controller.CursoController;
+import devandroid.edergularte.appgaseta.controller.PessoaController;
+import devandroid.edergularte.appgaseta.model.Pessoa;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    PessoaController controller;
+    CursoController cursoController;
+    Pessoa pessoa;
+    List<String> nomesDosCursos;
+    Pessoa outraPessoa;
+    EditText editPrimeiroNome;
+    EditText editSobreNome;
+    EditText editNomeCurso;
+    EditText editTelefoneContato;
+    Button btnLimpar;
+    Button btnSalvar;
+    Button btnFinalizar;
+    Spinner spinner;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_spinner);
+
+        controller = new PessoaController(MainActivity.this);
+        controller.toString();
+
+        cursoController = new CursoController();
+        nomesDosCursos = cursoController.dadosParaSpinner();
+
+        pessoa = new Pessoa();
+        controller.buscar(pessoa);
+
+        editPrimeiroNome = findViewById(R.id.editPrimeiroNome);
+        editSobreNome = findViewById(R.id.editSobreNome);
+        editNomeCurso = findViewById(R.id.editNomeCurso);
+        editTelefoneContato = findViewById(R.id.editTelefoneContato);
+        spinner = findViewById(R.id.spinner);
+
+        editPrimeiroNome.setText(pessoa.getPrimeiroNome());
+        editSobreNome.setText(pessoa.getSobreNome());
+        editNomeCurso.setText(pessoa.getCursoDesejado());
+        editTelefoneContato.setText(pessoa.getTelefoneContato());
+
+        btnLimpar = findViewById(R.id.btnLimpar);
+        btnSalvar = findViewById(R.id.btnSalvar);
+        btnFinalizar = findViewById(R.id.btnFinalizar);
+
+        //Adapter
+        //Layout
+        //Injetar Adapter ao Spinner - Lista será gerada
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,android.R.layout.simple_list_item_1, cursoController.dadosParaSpinner());
+        adapter.setDropDownViewResource(android.R.layout.simple_list_item_1);
+        spinner.setAdapter(adapter);
+
+        btnLimpar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editPrimeiroNome.setText("");
+                editSobreNome.setText("");
+                editNomeCurso.setText("");
+                editTelefoneContato.setText("");
+
+                controller.limpar();
+            }
+        });
+        btnFinalizar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Volte sempre!", Toast.LENGTH_LONG).show();
+                finish();
+            }
+        });
+
+        btnSalvar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                pessoa.setPrimeiroNome(editPrimeiroNome.getText().toString());
+                pessoa.setSobreNome(editSobreNome.getText().toString());
+                pessoa.setCursoDesejado(editNomeCurso.getText().toString());
+                pessoa.setTelefoneContato(editTelefoneContato.getText().toString());
+
+                Toast.makeText(MainActivity.this, "Salvo " + pessoa.toString(), Toast.LENGTH_LONG).show();
+
+                controller.salvar(pessoa);
+            }
+        });
+
+        Log.i("POOAndroid", "Objeto Pessoa: " + pessoa.toString());
+
+    }
+}
---
+++ app/src/main/java/devandroid/edergularte/appgaseta/view/SplashActivity.java
@@ -0,0 +1,36 @@
+package devandroid.edergularte.appgaseta.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import devandroid.edergularte.appgaseta.R;
+import devandroid.edergularte.appgaseta.database.GasEtaDB;
+
+public class SplashActivity extends AppCompatActivity {
+
+    public static final int TIME_OUT_SPLASH = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        comutarTelaSplash();
+    }
+
+    private void comutarTelaSplash() {
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+               GasEtaDB db = new GasEtaDB(SplashActivity.this);
+
+                Intent telaPrincipal = new Intent(SplashActivity.this, GasEtaActivity.class);
+                startActivity(telaPrincipal);
+                finish();
+            }
+        }, TIME_OUT_SPLASH);
+    }
+}
